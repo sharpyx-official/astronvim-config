4dcac14c2e3790c6d67479f9234aa97a2f609f10
sharpyx <sckopincev.artyom@gmail.com>
2023-03-21 23:35:44 +0400

    Changed neo-tree width. Success!

--- plugins/core.lua
+++ plugins/core.lua
@@ -24,15 +24,19 @@ return {
   -- { "max397574/better-escape.nvim", enabled = false },
   --
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  -- {
-  --   "L3MON4D3/LuaSnip",
-  --   config = function(plugin, opts)
-  --     require "plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-  --     -- add more custom luasnip configuration such as filetype extend or custom snippets
-  --     local luasnip = require "luasnip"
-  --     luasnip.filetype_extend("javascript", { "javascriptreact" })
-  --   end,
-  -- },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    -- config = function(plugin, opts)
+    -- require "plugins.configs.neo-tree"(plugin, opts) -- include the default astronvim config that calls the setup call
+    -- add more custom luasnip configuration such as filetype extend or custom snippets
+    -- local luasnip = require "luasnip"
+    -- luasnip.filetype_extend("javascript", { "javascriptreact" })
+    -- opts.window.size = 50
+    -- end,
+    opts = function(_, opts)
+      opts.window.width = 50
+    end
+  },
   -- {
   --   "windwp/nvim-autopairs",
   --   config = function(plugin, opts)
