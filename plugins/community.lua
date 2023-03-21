99247d6b9f7d6cdb7a357943a8ba86034cb0836e
sharpyx <sckopincev.artyom@gmail.com>
2023-03-22 00:04:22 +0400

    Added some colorschemes from community and changed to nithgfox.

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,13 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.nightfox" },
+  { import = "astrocommunity.colorscheme.kanagawa" },
+  { import = "astrocommunity.colorscheme.everforest" },
+  { import = "astrocommunity.colorscheme.gruvbox" },
+  { import = "astrocommunity.colorscheme.onigiri" },
+  { import = "astrocommunity.colorscheme.oxocarbon" },
+  { import = "astrocommunity.colorscheme.tokyonight" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
